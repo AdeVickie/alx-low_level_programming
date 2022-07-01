a669a4f65227f4666bc0e4303a6f24ea2404dcdc
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-01 16:42:37 -0700

    alx

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdlib.h>
 /**
-*main - main block
-*Description: Get a random number and check its last digit, compare it with 5
-*Return: 0
+* main - main block
+* Description: Get a random number and check its last digit, compare it with 5
+* Return: 0
 */
 int main(void)
 {
