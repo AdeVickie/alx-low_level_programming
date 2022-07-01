ed9daa11299e14ab298be4899f7033538859fd44
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-01 07:36:58 -0700

    alx

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/
+ * main - Entry point
+ *  * Return: Always 0 (sucess)
+ *   */
+ int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
