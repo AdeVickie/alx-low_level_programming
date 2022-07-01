58b5d01025fc689a2dd1a40742296c5788965c63
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-01 14:56:44 -0700

    alx

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
+
 /**
 *main -> assign a random number to the variable n each time it is executed
 *and print the last digit of the number stored in the variable n
