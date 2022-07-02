74eac664449e8377c93a75b755ffad233aa75f0c
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-01 18:09:25 -0700

    alx

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - Prints the last digit of a randomly generated number
-* and whether it is greater than 5, less than 6, or 0.
-* Return: Always 0.
+*main - Prints the last digit of a randomly generated number
+*and whether it is greater than 5, less than 6, or 0.
+*Return: Always 0.
 */
 int main(void)
 {
@@ -24,7 +24,7 @@ n % 10);
 }
 else
 {									
-printf("Last digit of %d is %d and is 0\n",n, 
+printf("Last digit of %d is %d and is 0\n", n, 
 n % 10);
 }
 return (0);
