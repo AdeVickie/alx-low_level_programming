0d078c55300d65ed5cb6b0fba08e4a1485e1d927
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-03 03:47:52 -0700

    alx

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,8 +9,10 @@ int num;
 for (num = 0; num <= 9; num++)
 {
 putchar((num % 10) + '0');
-if (num == 9)									continue;
-putchar(',');									putchar(' ');
+if (num == 9)
+continue;
+putchar(',');
+putchar(' ');
 }
 putchar('\n');
 return (0);
