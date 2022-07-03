9ba25cc4a989c7cf1b0148d1a0f1f773e97e2ebc
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-03 04:57:09 -0700

    alx

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,8 +11,10 @@ for (c = '0'; c <= '9'; c++)
 for (i = '0'; i <= '9'; i++)
 {										if (c < i)
 {										putchar(c);
-putchar(i);									if (c != '8' || (c == '8' && i != '9')
-{										putchar(',')									putchar(' ')									}																				}										}
+putchar(i);									if (c != '8' || (c == '8' && i != '9'))
+{
+putchar(',')									putchar(' ')									}										}
+}
 }
 putchar('\n');
 return (0);
