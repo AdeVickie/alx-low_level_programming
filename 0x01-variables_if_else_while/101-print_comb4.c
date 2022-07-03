55d7640c5ba1e9d8d10885c2d2f3d29686aa8d14
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-03 06:08:01 -0700

    alx

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,13 +8,14 @@ int main(void)
 int c, i, k;
 for (c = '0'; c <= '9'; c++)
 {
-for (i = '0'; i <= '9'; i++)	
+for (i = '0'; i <= '9'; i++)
 {
 for (k = '0'; k <= '9'; k++)
 {
 if (c < i && i < k)
 {
-putchar(c);								putchar(i);
+putchar(c);
+putchar(i);
 putchar(k);
 if (c != '7')
 {
@@ -22,7 +23,8 @@ putchar(',');
 putchar(' ');
 }
 }
-}									}
+}
+}								}
 }
 putchar('\n');
 return (0);
