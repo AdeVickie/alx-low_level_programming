fab2e5866d715bd8f13f162ef6b413c261c58563
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-03 05:22:30 -0700

    alx

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Prints combination of numbers
- * Return: Always (Success)
- */
+* main - Prints combination of numbers
+* Return: Always (Success)
+*/
 int main(void)
 {
 int c, i;
