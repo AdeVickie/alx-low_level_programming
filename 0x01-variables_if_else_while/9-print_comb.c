cbb5dbbbcc05b609729f2636e3e240c2e876585e
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-03 05:31:49 -0700

    alx

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Prints all possible combinations of single-digit numbers.
- * Return: Always 0.
- */
+* main - Prints all possible combinations of single-digit numbers.
+* Return: Always 0.
+*/
 int main(void)
 {
 int num;
