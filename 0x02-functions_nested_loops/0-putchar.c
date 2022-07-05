b11b242cefffb4991b17daf90d63a9fef1c23801
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-05 06:15:53 -0700

    alx

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,6 @@
-#include "main.h"
-/** main - prints putchar
- * Return: always 0
- */
+#include <stdio.h>
 int main(void)
 {
-	 _putchar("_");
-	 _putchar("p");
-	 _putchar("u");
-	 _putchar("t");
-	 _putchar("c");
-	 _putchar("a");
-	 _putchar("r");
-	 _putchar("\n")
-         return (0);
-}		
+("_putchar");
+return 0;
+}
