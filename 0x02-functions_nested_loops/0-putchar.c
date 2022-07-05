9e3ef320f8a0f27ee441af9d10aae0390268d530
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-05 09:31:00 -0700

    alx

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,17 @@
-#include <main.h>
-main.h
-
-int _putchar(int c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+#include "main.h"
+/**
+ * main - prints a string of characters
+ * Description: prints _putchar, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+char a[] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+_putchar(a[i]);
+}
+_putchar('\n');
+return (0);
+}
