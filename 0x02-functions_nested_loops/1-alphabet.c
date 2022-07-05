eddd80134fa55b922cc29b54ebac2f1ae21c6722
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-05 12:02:11 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints alphabet in lowercase
+ * Return: Nothing
+ */
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+}
