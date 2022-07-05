eef0cc0c63897e0a6949994d28adfe6c91452a18
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-05 12:51:57 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Check main
+ * @n: An input number
+ * Description: This function returns absolute value of a number
+ * Return: Absolute value of number n
+ */
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (n * -1);
+}
