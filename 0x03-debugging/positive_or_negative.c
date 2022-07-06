6575bf161a3503852b196b5329a6548a1056c689
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-06 16:44:21 -0700

    debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ *  * positive_or_negative(int i) - prints negative if number is less than 0
+ *
+ *   * prints positive if number is greater than 0
+ *
+ *    * prints zero if number is 0
+ *
+ *     * @i is the number to be printed
+ *
+ *      * Return Nothingi
+ *
+ *  
+
+}
+
+}
