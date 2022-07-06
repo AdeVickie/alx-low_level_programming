957a20f4a353fc5d8d866e043b10fd73a0e78629
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-06 16:58:00 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,13 +8,12 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
-/* while (i < 10) */
-
-/* { */
-
-/*	putchar(i); */
-
-/* } */
-printf("Infinite loop avoided! \\o/\n")
+/*
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
+ */
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
