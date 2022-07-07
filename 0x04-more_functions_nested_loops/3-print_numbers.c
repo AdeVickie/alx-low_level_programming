f0172510ecd97da613b5c12001c4cdfe44cf3bac
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-07 09:10:56 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9
+ * Return: 0-9 followed by newline
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+}
