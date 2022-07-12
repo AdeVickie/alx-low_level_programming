2bc5d2dce5e753eeab7d3fccfda6b55f01a9cde4
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-12 11:46:26 -0700

    Thank you Betty

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - prints pair values.
+ * @str: value to be evaluated.
+ * Return: 0
+ */
+void puts2(char *str)
+{
+int len = 0;
+int l = 0;
+char *y = str;
+int z;
+while (*y != '\0')
+{
+y++;
+len++;
+}
+l = len - 1;
+for (z = 0 ; z <= l ; z++)
+{
+if (z % 2 == 0)
+{
+_putchar(str[z])
+}
+}
+_putchar('\n');
+}
