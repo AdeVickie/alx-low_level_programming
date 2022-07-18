63ff29494946e89a96ea7f57c32f3024053523b4
Adedoyin <victoriaadedoyin7@gmail.com>
2022-07-18 16:56:59 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,10 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- * Return: Nothing.
+ *  * simple_print_buffer - prints buffer in hexa
+ *
+ *   * @buffer: the address of memory to print
+ *
+ *    * @size: the size of the memory to print
+ *
+ *     * Return: Nothing.
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
@@ -25,13 +28,14 @@ i++;
 }
 printf("\n");
 }
-
 /**
- * main - check the code
- * Return: Always 0.
+ * *  main - check the code
+ *
+ * *  Return: Always 0.
  */
 int main(void)
-{char buffer[98] = {0x00};
+{
+char buffer[98] = {0x00};
 simple_print_buffer(buffer, 98);
 _memset(buffer, 0x01, 95);
 printf("-------------------------------------------------\n");
--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,22 @@
 #include "main.h"
+
+
+
 /**
- * _memset - fill a block of memory with a specific value
- * @s: starting address of memory to be filled
- * @b: the desired value
- * @n: number of bytes to be changed
- * Return: changed array with new value for n bytes
+ * * _memset - fills memory with a constant byte
+ *
+ * * @s: pointer to memory area 's'
+ *
+ * * @b: constant byte to fill
+ *
+ * * @n: number of bytes to fill
+ *
+ * * Return: pointer to memory area 's'
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-int i = 0;
-while (n > 0)
-{
+unsigned int i;
+for (i = 0; i < n; i++)
 s[i] = b;
-i++;
-n--;
-}
 return (s);
 }
