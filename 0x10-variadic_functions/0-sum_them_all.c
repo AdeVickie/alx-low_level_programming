183a363e2a8aae5743b3fed24ad80a72d080795e
Adedoyin <victoriaadedoyin7@gmail.com>
2022-08-04 11:43:39 -0700

    Task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,17 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - returns the sum of all additional args
+ * @n: number of additional args
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+int sum = 0;
+va_list list;
+va_start(list, n);
+for (i = 0; i < n; i++)
+sum += va_arg(list, int);
+va_end(list);
+return (sum);
+}
